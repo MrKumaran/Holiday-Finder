1e53319d958a687b63e964a9f1744f8d6af48f23
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-02 19:01:59 +0530

    added year

--- app/src/main/java/com/holidayfinder/TitleBar.kt
+++ app/src/main/java/com/holidayfinder/TitleBar.kt
@@ -2,13 +2,16 @@ package com.holidayfinder
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,16 +25,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun TitleBar(location:String = "India") {
-    Row (
+fun TitleBar(location:String = "India", year:String = "2025") {
+    Row(
         modifier = Modifier
             .background(color = Color.White)
-            .padding(start = 16.dp, top = 28.dp, end = 16.dp, bottom = 16.dp)
+            .padding(start = 16.dp, top = 48.dp, end = 16.dp, bottom = 16.dp)
             .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
-    ){
+    ) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         Image(
-            imageVector = Icons.Outlined.LocationOn,
+            imageVector = Icons.Outlined.Place,
             contentDescription = "Location Icon",
             modifier = Modifier
                 .height(28.dp),
@@ -39,7 +44,7 @@ fun TitleBar(location:String = "India") {
         )
         Spacer(
             modifier = Modifier
-                .padding(horizontal = 4.dp)
+                .padding(horizontal = 2.dp)
         )
         Text(
             text = location,
@@ -48,4 +53,24 @@ fun TitleBar(location:String = "India") {
         )
 
     }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            imageVector = Icons.Outlined.DateRange,
+            contentDescription = "Location Icon",
+            modifier = Modifier
+                .height(28.dp),
+            contentScale = ContentScale.FillHeight
+        )
+        Spacer(
+            modifier = Modifier
+                .padding(horizontal = 2.dp)
+        )
+        Text(
+            text = year,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 24.sp
+        )
+
+    }
+}
 }
