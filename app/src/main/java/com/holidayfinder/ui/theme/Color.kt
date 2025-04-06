1abe0ae93b651cb0a903c49c9753ed13094fe06b
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-06 17:25:37 +0530

    Added colors

--- app/src/main/java/com/holidayfinder/ui/theme/Color.kt
+++ app/src/main/java/com/holidayfinder/ui/theme/Color.kt
@@ -2,11 +2,13 @@ package com.holidayfinder.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-val littleWhite = Color(0xFFFFFFFF)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Cyan = Color(0xFF00B8D4)
+val Transparent = Color(0x00FFFFFF)
+val Filter_50White = Color(0x80FFFFFF)
+val Filter_50Black = Color(0x80000000)
+val Filter_50Cyan = Color(0x8000B8D4)
+val Filter_70White = Color(0xB3FFFFFF)
+val Filter_70Black = Color(0xB3000000)
+val Filer_70Cyan = Color(0xB300B8D4)
